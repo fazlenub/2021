52dc24ab1bae896fab4194f2fff1d0d9a14617b1
Rabbi <mdfazlerabbi06@gmail.com>
2021-04-07 21:27:07 +0600

    07-04-2021

--- Project-1 Cuda/PSD TO HTML/resources/js/main.js
+++ Project-1 Cuda/PSD TO HTML/resources/js/main.js
@@ -14,3 +14,10 @@ $(document).ready(function(){
     //MIXITUP Portfolio section
     var mixer = mixitup('.container');  
 });
+
+function openNav(){
+    document.getElementById("myNav").style.width ="100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width ="0";
+}
